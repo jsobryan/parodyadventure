fda9868f01e98d8cb741fcc48e7020efa17595a1
jsobryan <jsobryan@gmail.com>
2022-05-19 19:09:55 -0500

    adding player skill choice

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 import random
 import os
 
+from numpy import choose
+
+#because f strings don't like backslashes
+newline = '\n'
 #player / enemy stats
 class Player:
     def __init__(self,name,hp):
@@ -18,17 +22,6 @@ class Enemy:
         self.successtext = []
         self.failtext = []
 
-#skills.  Need to add player choice logic
-skill_list = [
-    "Hide",
-    "Flee",
-    "Cower",
-    "Nutpunch",
-    "Disgust",
-    "Beg",
-    "Flail",
-]
-
 #example encounter stats
 p1 = Player("Florbert The Unseemly",20)
 p1.skills = ["hide","cower","flail"]
@@ -37,8 +30,34 @@ donkey.successtext = "It looks at you with what appears to be confusion and slow
 donkey.failtext = "It tramples you.  You die."
 donkey.resistances = "cower"
 donkey.weaknesses = "flail"
+# skills.  Need to add player choice logic
+skilllist = [
+    "Hide",
+    "Flee",
+    "Cower",
+    "Nutpunch",
+    "Beg",
+    "Flail",
+]
 
-#game mechanics
+# game mechanics
+def yourchoice():
+    return int(input("Your choice: "))
+
+def chooseskills():
+    while len(p1.skills) <= 3:
+        for index, value in enumerate(skilllist, start=1):
+            print(f'{index}: {value}')
+        choice = input("Choose 3 Skills (press k for skill descriptions): ")
+        if choice.lower() == 'k':
+            for item in skilllist:
+                print(f'{item}: {item.skilltext}')
+        elif choice in skilllist:
+            p1.skills.append(skilllist.pop(skilllist.index(choice-1)))
+        else:
+            print('Not a valid response')
+            continue
+
 class Encounter:
     def __init__(self,encounternum):
         self.encounternum = encounternum
@@ -46,16 +65,15 @@ class Encounter:
         nextencounter = encounternum + 1
         prevencounter = encounternum - 1
 
-def yourchoice():
-    return int(input("Your choice: "))
-
 class Skill:
     def __init__(self,skillname):
         self.skillname = skillname
-        self.skilltext = []
+        self.skilltext = " "
         self.skilloutcomes = []
 
-#skill descriptions
+# skill descriptions
+flee = Skill("flee")
+flee.skilltext = "You attempt to run away from the target.  This may result in returning to the previous encounter."
 
 hide = Skill("hide")
 hide.skilltext = "You frantically attempt to hide under the nearest available cover."
@@ -68,48 +86,40 @@ flail.skilloutcomes = [
         "You raise your hands above your head in a threatening gesture and flail at the ",
     ]
 
-disgust = Skill("Disgust")
+disgust = Skill("disgust")
 disgust.skilltext = "You perform some form of cowardly act involving bodily fluids."
 
+beg = Skill("beg")
+beg.skilltext = "You get on your knees and pathetically beg for the target not to harm you."
 
-#game encounters
-def encounter1():
-    print(f'''
-    Walking down the path, you see a large gray donkey in the middle of the road.  It doesn't appear to be interested in moving any time soon.
+cower = Skill("cower")
+cower.skilltext = ("You attempt to make yourself as non-threatening a target as possible, curling into a ball and cowering in fear.")
 
-    If you choose to flee from the donkey and return the way you came, press 1
-    If you choose to hide in the bushes beside the road until it leaves, press 2
-    If you choose to flail wildly against the donkey with your fists, press 3
-    ''')
-    while True:
-        choice = yourchoice()
-        if choice == 1:
-            print('You turn away and go back the way you came')
-            prevencounter()
-        elif choice == 2:
-            print('You wait for several hours in the bug-infested reeds.  The donkey does not move')
-            continue
-        elif choice == 3:
-            print(f'{flail()}.  It looks at you with what appears to be confusion and slowly saunters down the road')
-            nextencounter()
+nutpunch = Skill("nutpunch")
+nutpunch.skilltext = ("You actually attempt to attack the target by punching it in the nuts or other apparently vulnerable area.")
+
+# game encounters
+e1 = Encounter(1)
+e1.encountertext = f'''
+Walking down the path, you see a large gray donkey in the middle of the road.  It doesn't appear to be interested in moving any time soon.
+'''
 
 #testing
 
-while True:
-    print("\n".join(p1.skills))
-    x = input("What do you do? : ")
-    if x in donkey.weaknesses:
-        print(donkey.successtext)
-        break
-    elif x in donkey.resistances:
-        print(donkey.failtext)
-        break
-    else:
-        print("nothing happens")
-        continue
+# while True:
+#     print("\n".join(p1.skills))
+#     x = input("What do you do? : ")
+#     if x in donkey.weaknesses:
+#         print(donkey.successtext)
+#         break
+#     elif x in donkey.resistances:
+#         print(donkey.failtext)
+#         break
+#     else:
+#         print("nothing happens")
+#         continue
 
-
-
+chooseskills()
 
 
 
