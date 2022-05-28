40727546d21e03822161858432fff6f19f66c7de
jsobryan <jsobryan@gmail.com>
2022-05-28 17:29:22 -0500

    updating encounter class

--- test.py
+++ test.py
@@ -11,8 +11,6 @@ import os
 from re import X
 from texts import suffix
 
-
-
 #because f strings don't like backslashes
 newline = '\n'
 #player / enemy stats
@@ -30,6 +28,22 @@ class Player:
     def sethp(self, newhp):
         self.health = newhp
 
+    def getgold(self):
+        return self.gold
+
+    def setgold(self, newgold):
+        self.gold = newgold
+
+    def equipment(self):
+        return self.equipment
+
+    def equipment(self, newequip):
+        self.equipment = newequip
+
+    def getskills(self):
+        text = ", ".join(self.skills)
+        return text
+
 class Enemy:
     def __init__(self,name):
         self.name = name
@@ -37,11 +51,9 @@ class Enemy:
         self.weaknesses = []
         self.successtext = []
         self.failtext = []
+        self.failskill = []
         self.specialfail = []
-
-
-
-
+        
 # skills.  Need to add player choice logic
 skilllist = [
     "Hide",
@@ -60,7 +72,7 @@ def name():
 def playername():
     return f'{name()} the {random.choice(suffix)}'
 p1 = Player(playername,20)
-
+p1.skills = ["hide","cower","flail","nutpunch"]
 
 def yourchoice():
     return int(input("Your choice: "))
@@ -84,8 +96,8 @@ class Encounter:
         self.encounternum = encounternum
         self.encountertext = ""
         self.choices = []
-        self.skillchoices = p1.skills
-
+        self.skillchoices = p1.getskills()
+    
     # def use_skill(self, skillname):
     #     self.use_skill = self.skillname
     #     self.skillname = Skill.skillname
@@ -97,26 +109,51 @@ class Encounter:
     #     else:
     #         text = skillname.nope()
 
-    def choiceopts(self, choice):
-        choice = self.choices
+    def choice(self, x,y):
+        response = int(input("Your choice: "))
+        if response == 1:
+            self.runencounter(x)
+        elif response == 2:
+            self.runencounter(y)    
         
-    def runencounter(self):
+    def runencounter(self,enemy):
+        @property
+        def choices(self):
+            choice = int(input("Your choice?: "))
+            self.choices[choice]
         print(self.encountertext)
-        print(self.skillchoices)
+        print(f'Your Skills: {self.skillchoices}')
+        answer = input("Your Choice: ")
+        if answer in p1.skills:
+            answer.useon(enemy)
+
 
 class Skill:
     def __init__(self,skillname):
         self.skillname = skillname
         self.skilltext = " "
         self.skilloutcomes = []
-        self.noeffect = []
+        self.noeffects = []
 
-    def outcome(skillname):
-        return random.choice(skillname.skilloutcomes)
-    def nope(skillname):
-        return random.choice(skillname.noeffect)
-
+    def use(self,enemy):
+        self.enemy = enemy
+        text = random.choice(self.skilloutcomes)
+        return text
     
+    def useon(self,enemy):
+        text = None
+        if self.skillname in enemy.weaknesses:
+            text = enemy.successtext
+        elif self.skillname in enemy.resistances:
+            text = enemy.failtext
+        elif self.skillname in enemy.specialfail:
+            text = enemy.specialfail
+        else:
+            text = self.noeffects
+        return text
+        
+    def nope(self):
+        return random.choice(self.noeffect)
 
 # skill descriptions
 flee = Skill("flee")
@@ -129,7 +166,7 @@ hide.skilloutcomes = [
     "Your eyes widen with terror and you scramble for a suitable place to hide.",
     "In a panicked attempt to conceal yourself, you dive under the nearest available hiding place.",
 ]
-hide.noeffect = [
+hide.noeffects = [
 
 ]
 
@@ -139,7 +176,7 @@ flail.skilloutcomes = [
         "You hammer your fists wildly at the ",
         "You let loose an anguished cry and flail at the ",
         "You raise your hands above your head in a threatening gesture and flail at the ",
-        "You screech like an enraged child and aggressively thump your balled fists against the #ENEMYNAME#",
+        "You screech like an enraged child and aggressively thump your balled fists against the {enemy.name}",
         "Making a noise that bears an uncanny resemblance to a broken teakettle, you lash out wildly with your fists."
     ]
 
@@ -154,7 +191,7 @@ disgust.skilloutcomes = [
     "You double over and vomit on the ground at the sight of the #ENEMYNAME#",
     "A sound like tearing paper fills the air as you are wracked with several violent sharts.  Shortly thereafter, a tremendous explosion of watery feces fills your pants."
 ]
-
+disgust.noeffects = "The putrid stench of your own bodily fluids surrounds you, but the enemy.name simply stares at you with incomprehension."
 beg = Skill("beg")
 beg.skilltext = "You get on your knees and pathetically beg for the target not to harm you."
 beg.skilloutcomes = [
@@ -178,18 +215,22 @@ nutpunch = Skill("nutpunch")
 nutpunch.skilltext = ("You actually attempt to attack the target by punching it in the nuts or other apparently vulnerable area.")
 nutpunch.skilloutcomes = [
     "You awkwardly cock your fist back and unleash a reckless haymaker",
-    "Aiming for the private parts of the #ENEMYNAME#, you swing wildly with your balled fist.",
+    "Aiming for the private parts of the , you swing wildly with your balled fist.",
     "Seeing an opportunity to fight as dirty as possible, you throw a punch aimed at the target's genitals."
 ]
 
 # game encounters
-e2 = Encounter(1)
-e2.encountertext = f'''
+e1 = Encounter(1)
+e1.encountertext = f'''
 You wander carefully through the smoking wreckage of your former home.  As you near the outskirts of Florpflump, you are startled by the sounds of a plaintive cry for help.  It appears to be coming from behind a nearby woodpile.
 
+
 If you choose to investigate, press 1.
+
 If you choose to ignore the noise and continue down the lane leading out of Florpflump, press 2.
 '''
+
+
 
 e2 = Encounter(2)
 e2.encountertext = f'''
@@ -213,102 +254,106 @@ e3.encountertext = f'''
 Walking down the path, you see a large gray donkey in the middle of the road.  It doesn't appear to be interested in moving any time soon.
 '''
 
-e4 = Encounter(4)
-def notecheck():
-    if "crumpled note" in p1.equipment:
-        additionaltext = 'As you ponder which way to go, you remember the note that Scrapdapple gave you.  Although nearly illegible, you manage to make out the words, "treasure" and "grove"'
-        return additionaltext
-    else:
-        pass
-e4.encountertext = f'''
-With the donkey out of the way, you continue to walk down the path.  After some time, you come to a crossroads.  The westward path looks to lead toward a large grove of trees far in the distance.  The Eastward path winds around a large hill.
+# e4 = Encounter(4)
+# def notecheck():
+#     if "crumpled note" in p1.equipment:
+#         additionaltext = 'As you ponder which way to go, you remember the note that Scrapdapple gave you.  Although nearly illegible, you manage to make out the words, "treasure" and "grove"'
+#         return additionaltext
+#     else:
+#         pass
+# e4.encountertext = f'''
+# With the donkey out of the way, you continue to walk down the path.  After some time, you come to a crossroads.  The westward path looks to lead toward a large grove of trees far in the distance.  The Eastward path winds around a large hill.
 
-{notecheck()}
+# {notecheck()}
 
-If you take the westward path towards the grove, press 1
-If you take the Eastward path toward the hill, press 2
-If you choose to continue north, press 3
-'''
+# If you take the westward path towards the grove, press 1
+# If you take the Eastward path toward the hill, press 2
+# If you choose to continue north, press 3
+# '''
 
-e5 = Encounter(5)
-e5.encountertext = f'''
-You head westward towards the trees.  The hot afternoon sun beats down upon you as you trod the dusty path into the grove.  Welcoming the shade offered by the trees, you pause and rest upon a nearby log.  Suddenly, you hear a crashing noise behind you, and you turn to find yourself face to face with a large group of Ogres!  They stare at you with wicked amusement as drool drips slowly from their enormous, fanged mouths.
+# e5 = Encounter(5)
+# e5.encountertext = f'''
+# You head westward towards the trees.  The hot afternoon sun beats down upon you as you trod the dusty path into the grove.  Welcoming the shade offered by the trees, you pause and rest upon a nearby log.  Suddenly, you hear a crashing noise behind you, and you turn to find yourself face to face with a large group of Ogres!  They stare at you with wicked amusement as drool drips slowly from their enormous, fanged mouths.
 
-'Well, well, well...  Wot 'ave we 'ere?  One of the Ogres says menacingly.  'a lil' lost lamb all alone in the forest?  It's been a long time since we 'ad lamb for supper!  
+# 'Well, well, well...  Wot 'ave we 'ere?  One of the Ogres says menacingly.  'a lil' lost lamb all alone in the forest?  It's been a long time since we 'ad lamb for supper!  
 
-They begin to move slowly toward you, brandishing long, jagged knives.
-'''
-def finaljoke():
-    if "crumpled note" in p1.equipment:
-        additionaltext = 'You realize, too late, that Scrapdapple has played one final, cruel joke upon you, and that he must have written the note as he saw you approaching.  There is no treasure here, only your certain doom.'
-        return additionaltext
-    else:
-        pass
-ogre = Enemy("ogre")
-ogre.resistances = ["cower","hide","flee","nutpunch","flail","beg"]
-ogre.weaknesses = ["disgust"]
+# They begin to move slowly toward you, brandishing long, jagged knives.
+# '''
+# def finaljoke():
+#     if "crumpled note" in p1.equipment:
+#         additionaltext = 'You realize, too late, that Scrapdapple has played one final, cruel joke upon you, and that he must have written the note as he saw you approaching.  There is no treasure here, only your certain doom.'
+#         return additionaltext
+#     else:
+#         pass
+# ogre = Enemy("ogre")
+# ogre.resistances = ["cower","hide","flee","nutpunch","flail","beg"]
+# ogre.weaknesses = ["disgust"]
 
-if e5.use_skill(cower):
-    ogre.failtext = f'''
-    {cower.outcome} 
+# if e5.use_skill(cower):
+#     ogre.failtext = f'''
+#     {cower.outcome} 
 
-    {finaljoke()} The last thing you hear is the Ogres laughing at your cowardice as they hack you to pieces'''
-elif hide.use_skill:
-    ogre.failtext = f'''
-    {hide.outcome} 
+#     {finaljoke()} The last thing you hear is the Ogres laughing at your cowardice as they hack you to pieces'''
+# elif hide.use_skill:
+#     ogre.failtext = f'''
+#     {hide.outcome} 
 
-    {finaljoke()} Your attempts to hide, however, are futile.  You have survived the massacre of your home only to end up in an Ogre's stewpot.'''
-elif flee.use_skill:
-    ogre.failtext = f'''
-    {flee.outcome} 
+#     {finaljoke()} Your attempts to hide, however, are futile.  You have survived the massacre of your home only to end up in an Ogre's stewpot.'''
+# elif flee.use_skill:
+#     ogre.failtext = f'''
+#     {flee.outcome} 
 
-    {finaljoke()} You manage to duck between the legs of one of the Ogres and desperately run toward the edge of the grove, only to feel a hot flash of pain as long, thick claws dig mercilessly into your back.'''
-elif nutpunch.use_skill:
-    ogre.failtext = f'''
-    {nutpunch.outcome} 
+#     {finaljoke()} You manage to duck between the legs of one of the Ogres and desperately run toward the edge of the grove, only to feel a hot flash of pain as long, thick claws dig mercilessly into your back.'''
+# elif nutpunch.use_skill:
+#     ogre.failtext = f'''
+#     {nutpunch.outcome} 
 
-    {finaljoke()} The ogres look at you with amusement as your blow goes wide.  The grove rings with their hideous laughter.
-    'Oooh, This'un 'ere's a fiesty one!' One of the Ogres cackles.  'This will be some spicy stew!'
+#     {finaljoke()} The ogres look at you with amusement as your blow goes wide.  The grove rings with their hideous laughter.
+#     'Oooh, This'un 'ere's a fiesty one!' One of the Ogres cackles.  'This will be some spicy stew!'
 
-    '''
-elif disgust.use_skill:
-    ogre.successtext = f'''
-    {finaljoke()}
+#     '''
+# elif disgust.use_skill:
+#     ogre.successtext = f'''
+#     {finaljoke()}
 
-    {disgust.outcome}
+#     {disgust.outcome}
 
-    The Ogres look upon you with disgust and horror.  'Ewwww, this'un's messed 'imself!  The head Ogre shouts.  'By Grokflarp's ragged teats, the smell is awful!  We'll never be able to clean 'im up!  Let's get out of 'ere!
+#     The Ogres look upon you with disgust and horror.  'Ewwww, this'un's messed 'imself!  The head Ogre shouts.  'By Grokflarp's ragged teats, the smell is awful!  We'll never be able to clean 'im up!  Let's get out of 'ere!
 
-    You breathe a sigh of relief, barely noticing the stench of your own filth as the Ogres tromp away deeper into the woods.
-    '''
+#     You breathe a sigh of relief, barely noticing the stench of your own filth as the Ogres tromp away deeper into the woods.
+#     '''
 
 
 #example encounter stats
 # p1 = Player("Florbert The Unseemly",20)
-p1.skills = ["hide","cower","flail"]
+
+e6 = Encounter(6)
+e6.encountertext = '''
+You make your way down the path towards the hill, you notice a figure in the distance, apparently walking the same direction you are going. As you approach the foot of the hill, you realize it is and old beggar-man with a large knapsack.  He holds a hand out and asks,
+
+'Hello, fellow traveler, care to spare a penny for an old man?'
+'''
+###
+# flail or nutpunch - "The old beggar stumbles back with a startled look and falls flat on his back.  You proceed, confident that you have vanquished what was certainly a dangerous foe."
+
+# hide / cower / disgust - "The old man looks at you with puzzlement and then chuckles heartily.  'umm, here, take this, looks like you need it more than me."
+
+# flee - 'you run screaming from the sight of the old beggar man.'
+
+
+###
+
 donkey = Enemy("donkey")
+donkey.failskill = nutpunch
 donkey.successtext = "It looks at you with what appears to be confusion and slowly saunters down the road"
 donkey.failtext = "It tramples you.  You die."
 donkey.specialfail = "You punch the donkey's balls with all your might.  It makes a startled cry and kicks you in the head. It then runs away from you down the path.  You may proceed, but lose 4 HP"
 donkey.resistances = "cower"
 donkey.weaknesses = "flail"
+
 #######
 
 #testing
-# while True:
-#     print("\n".join(p1.skills))
-#     x = input("What do you do? : ")
-#     if x in donkey.weaknesses:
-#         print(donkey.successtext)
-#         break
-#     elif x in donkey.resistances:
-#         print(donkey.failtext)
-#         break
-#     else:
-#         print("nothing happens")
-#         continue
-
-
-
+e3.runencounter(donkey)
 
 
